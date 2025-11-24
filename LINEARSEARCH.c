b842e7e29c1e35aa97cf9face67c050c1bc9cd0d
11249a152-code <11249a152@kanchiuniv.ac.in>
2025-11-24 08:30:58 -0800

    Update Linear search.c

--- LINEARSEARCH.c
+++ LINEARSEARCH.c
@@ -1,32 +1,34 @@
+//AIM:- To perform Linear Search operation in DSA using C programming
+
 #include<stdio.h>
 #include<stdbool.h>
 int main()
 {
-  int i, n, key;
-  printf("Enter the size of array\n");
-  scanf("%d", &n);
-  int arr[n];
-  printf("Enter the elements in array\n");
-  for (i = 0; i < n; i++)
-  {
-    scanf("%d", &arr[i]);
-  }
-  printf("Enter the value to be searched\n");
-  scanf("%d", &key);
-  bool flag;
-  flag = false;
-  for (i = 0; i < n; i++)
-  {
-    if (arr[i] == key)
+    int i,n,key;
+    printf("Enter the size of array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements in array\n");
+    for(i=0;i<n;i++)
     {
-      printf("Element found at position %d, %d\n", i, arr[i]);
-      flag = true;
-      break;
+        scanf("%d",&arr[i]);
     }
-  }
-  if (!flag)
-  {
-    printf("Element not found\n");
-  }
-  return 0;
+    printf("Enter the value to be searched\n");
+    scanf("%d",&key);
+    bool flag;
+    flag=false;
+    for(i=0;i<n;i++)
+    {
+       if(arr[i]=key)
+       {
+           printf("Element found at position %d,%d\n",arr[i]);
+           flag=true;
+           break;
+       }
+    }
+    if(flag)
+    {
+         printf("Element not found");
+    }
+    return 0;
 }
